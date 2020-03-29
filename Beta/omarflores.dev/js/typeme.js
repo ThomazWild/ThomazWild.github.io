d9e8b6ae2aeb621906626503fbb6c53db95007ee
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-03-29 21:38:27 +1100

    Bad

---
+++ Beta/omarflores.dev/js/typeme.js
@@ -0,0 +1,9 @@
+var typed = $(".typed");
+
+$(() => {
+	typed.typed({
+	  strings: ["programming.", "designing.",  "singing.", "playing guitar.", "waiting for a bus."],
+	  typeSpeed: 140,
+	  loop: true,
+	});
+});
