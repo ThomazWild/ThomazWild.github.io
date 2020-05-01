896e5de9fbc1d6aaeec4b56ee8f008d985c704b9
Thomaz <50690864+ThomazWild@users.noreply.github.com>
2020-05-02 04:30:45 +1000

    Big performance update

--- Beta/omarflores.dev/js/typeme.js
+++
@@ -1,9 +0,0 @@
-var typed = $(".typed");
-
-$(() => {
-	typed.typed({
-	  strings: ["programming.", "designing.",  "singing.", "playing guitar.", "waiting for a bus."],
-	  typeSpeed: 140,
-	  loop: true,
-	});
-});
